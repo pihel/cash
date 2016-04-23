ab2826403a9e30c1e55a90dde919b3fee972be38
Alexey AL <AlexeySa@users.noreply.github.com>
2016-04-23 15:22:29 +0300

    Update usr_list_analiz.php

--- ajax/analiz/usr_list_analiz.php
+++ ajax/analiz/usr_list_analiz.php
@@ -1,10 +1,10 @@
-<?
+<?php
 require_once("../../lib/init.php");
 require_once("../../lib/settings.php");
 
 $ch_set = new CashSett($db, $usr, $lng);
 $names = $ch_set->getUsrNames( $usr->db_id );
-array_unshift(&$names, array('id' => -1, 'name' => $lng->get(202) ));
+array_unshift($names, array('id' => -1, 'name' => $lng->get(202) ));
 echo json_encode( $names );
-?>
+
 
